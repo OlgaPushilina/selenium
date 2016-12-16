d7e0b4c119e695e9f80e29f9d5683b746892ed08
OlgaPushilina <olya.pushilina@gmail.com>
2016-12-15 19:05:32 -0800

    Сделана заготовка теста

---
+++ src/test/java/ru/stqa/selenium/page_objects/CustomerRegistrationTests.java
@@ -0,0 +1,12 @@
+package ru.stqa.selenium.page_objects;
+
+import org.junit.Test;
+import ru.stqa.selenium.page_objects.TestBase;
+
+public class CustomerRegistrationTests extends TestBase {
+
+  @Test
+  public void canRegisterCustomer() {
+
+  }
+}
