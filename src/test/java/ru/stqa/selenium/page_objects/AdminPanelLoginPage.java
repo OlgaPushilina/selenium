b0d4e3ff228a2d75bc757e652a90967201c7ba1d
OlgaPushilina <olya.pushilina@gmail.com>
2016-12-16 14:42:53 -0800

    Часть локаторов перенесена в объект-страницу

---
+++ src/test/java/ru/stqa/selenium/page_objects/AdminPanelLoginPage.java
@@ -0,0 +1,10 @@
+package ru.stqa.selenium.page_objects;
+
+import org.openqa.selenium.WebDriver;
+
+public class AdminPanelLoginPage extends Page {
+
+  public AdminPanelLoginPage(WebDriver driver) {
+    super(driver);
+  }
+}
---
+++ src/test/java/ru/stqa/selenium/page_objects/CustomerListPage.java
@@ -0,0 +1,11 @@
+package ru.stqa.selenium.page_objects;
+
+import org.openqa.selenium.WebDriver;
+
+public class CustomerListPage extends Page {
+
+  public CustomerListPage(WebDriver driver) {
+    super(driver);
+  }
+
+}
